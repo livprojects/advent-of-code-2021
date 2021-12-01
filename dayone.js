d230f0fa6fbde32758b933c641d926248cbc573d
livprojects <liv.audigane@gmail.com>
2021-12-01 12:22:36 +0100

    part two (with help)

--- dayone.js
+++ dayone.js
@@ -1,11 +1,11 @@
-// FIND HOW MANY MEASUREMENTS ARE GREATER THAN THE ONE BEFORE
+// PART ONE - FIND HOW MANY MEASUREMENTS ARE GREATER THAN THE ONE BEFORE
+
 const fs = require('fs');
-const inputs = fs.readFileSync('inputdayone.txt').toString().split("\r\n");
+const inputs = fs.readFileSync('inputdayone.txt').toString().split("\r\n").map(Number);
 
 console.log(inputs);
 
 let biggerCounter = 0;
-
 inputs.forEach((element, index, array) => {
 
     if (parseInt(element, 10) > array[index -1]) {
@@ -13,4 +13,24 @@ inputs.forEach((element, index, array) => {
     }
 
 });
-console.log("TOTAL " + biggerCounter);
+
+//PART 2 - SAME BUT WITH SUMS OF THREE VALUES 
+
+const sumCounter = inputs.reduce((increment, curr, index, source) => {
+    const previousSum = source[index - 1] + source[index] + source[index + 1];
+    const currentSum = source[index] + source[index + 1] + source[index + 2];
+  
+    if (index === 0) {
+        return 0;
+    }
+    if (currentSum > previousSum) {
+        return increment + 1;
+    }
+    
+    return increment;
+  }, 0);
+
+  
+
+console.log("TOTAL INCREMENTS: " + biggerCounter);
+console.log("TOTAL SUM INCREMENTS: " + sumCounter);
